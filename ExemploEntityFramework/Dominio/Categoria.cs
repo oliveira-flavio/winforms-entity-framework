7f12cadfa5b541e75558698917ff58bd741b97ae
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-08-02 20:27:49 -0300

    Adiciona classe categoria

---
+++ ExemploEntityFramework/Dominio/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploEntityFramework.Dominio
+{
+    class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Nome { get; set; }
+    }
+}
